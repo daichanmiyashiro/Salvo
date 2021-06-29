27a31f8da3a4001e07e55d6b9e9e227b777b4e30
daichanmiyashiro <47321602+daichanmiyashiro@users.noreply.github.com>
2021-06-29 18:23:39 -0300

    CreacionGame
    Clase Game -Repository

--- src/main/java/com/codeoftheweb/Salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/Salvo/SalvoApplication.java
@@ -1,7 +1,15 @@
 package com.codeoftheweb.Salvo;
-
+import com.codeoftheweb.Salvo.models.Game;
+import com.codeoftheweb.Salvo.models.GameRepository;
+import com.codeoftheweb.Salvo.models.Player;
+import com.codeoftheweb.Salvo.models.PlayerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @SpringBootApplication
 public class SalvoApplication {
@@ -9,6 +17,25 @@ public class SalvoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SalvoApplication.class, args);
 	}
-	
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository repository, GameRepository gameRepo) {
+		return (args) -> {
+
+			repository.save(new Player("j.bauer@ctu.gov"));
+			repository.save(new Player("c.obrian@ctu.gov"));
+			repository.save(new Player("kim_bauer@gmail.com"));
+			repository.save(new Player("t.almeida@ctu.gov"));
+
+			Date date1 = new Date();
+			Date date2 = Date.from(date1.toInstant().plusSeconds(3600));
+			Date date3 = Date.from(date1.toInstant().plusSeconds(7200));
+			gameRepo.save(new Game(LocalDateTime.now()));
+			gameRepo.save(new Game(LocalDateTime.now().plusHours(1)));
+			gameRepo.save(new Game(LocalDateTime.now().plusHours(2)));
+
+
+		};
+	}
 
 }
---
+++ src/main/java/com/codeoftheweb/Salvo/models/Game.java
@@ -0,0 +1,34 @@
+package com.codeoftheweb.Salvo.models;
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+
+    private LocalDateTime fechaCreacion;
+
+    public Game() {
+    }
+
+    public Game(LocalDateTime date) {
+        this.fechaCreacion = date;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+}
